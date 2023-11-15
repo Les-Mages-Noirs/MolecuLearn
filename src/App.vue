93531e58949ad3fe87b49faf7f7cec8622c9db56
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2023-11-15 12:15:52 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-screen h-screen bg-black flex justify-center items-center">
+    <div class="font-bold h-10 text-white">TEST</div>
+  </div>
+</template>
