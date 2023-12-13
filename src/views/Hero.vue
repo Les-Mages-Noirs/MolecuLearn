f9a25b105b2acd3431a1cb075e633b986228bd80
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2023-12-13 17:45:33 +0100

    debut hero

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -3,11 +3,11 @@ import heroGradient from "../assets/hero-gradient.svg";
 </script>
 
 <template>
-  <div class="w-screen h-screen flex">
-    <div class="font-semibold h-10 text-9xl font-montserrat z-10 ml-20">
+  <div class="w-screen h-screen flex relative">
+    <div class="font-semibold h-10 text-9xl font-montserrat z-10 ml-20 mt-80">
       MOLECULEARN
     </div>
-    <div class="absolute z-0 right-0">
+    <div class="absolute z-0 right-[-5%]">
       <heroGradient />
     </div>
   </div>
