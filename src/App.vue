46583bff256cb1f5cbe88c392b5c4b1fe72fea8f
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2023-12-13 16:00:26 +0100

    refactor folders

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-screen h-screen bg-black flex justify-center items-center">
-    <div class="font-bold h-10 text-white">TEST</div>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div class="font-bold h-10">MOLECULEARN</div>
   </div>
 </template>
