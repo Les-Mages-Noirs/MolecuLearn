a435a32f594f873a6d552ef37a2e5b8725c26da6
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2024-01-20 16:32:32 +0100

    Update brand name in Hero.vue

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -8,7 +8,7 @@
     <img src="../../public/img/science.jpeg" class="max-w-4xl" />
     <div>
       <h1 class="text-8xl font-bold">MOLECULEARN</h1>
-      <p class="py-6 text-2xl">Découvrez <strong>Moleculearn</strong>, votre laboratoire virtuel pour explorer le monde fascinant des atomes dans les molécules. </p>
+      <p class="py-6 text-2xl">Découvrez <strong class="text-primary" >MolecuLearn</strong>, votre laboratoire virtuel pour explorer le monde fascinant des atomes dans les molécules. </p>
       <div class="space-x-6">
         <button class="btn btn-primary">Jouer</button>
         <button class="btn">Recettes</button>
