a2a3ba48e607b764ce86023c22068285c1acb514
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2024-01-21 21:33:57 +0100

    Update Hero.vue with responsive design

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -5,10 +5,10 @@
 <template>
   <div class="hero min-h-[calc(100vh-4rem)]">
   <div class="hero-content flex-col lg:flex-row-reverse">
-    <img src="../../public/img/science.jpeg" class="max-w-4xl" />
+    <img src="../../public/img/science.jpeg" class="2xl:max-w-4xl max-w-xl" />
     <div>
-      <h1 class="text-8xl font-bold">MOLECULEARN</h1>
-      <p class="py-6 text-2xl">Découvrez <strong class="text-primary" >MolecuLearn</strong>, votre laboratoire virtuel pour explorer le monde fascinant des atomes dans les molécules. </p>
+      <h1 class="text-6xl font-bold lg:text-8xl">MOLECULEARN</h1>
+      <p class="py-6 text-xl lg:text-2xl w-2/3 lg:w-full">Découvrez <strong class="text-primary" >MolecuLearn</strong>, votre laboratoire virtuel pour explorer le monde fascinant des atomes dans les molécules. </p>
       <div class="space-x-6">
         <button class="btn btn-primary">Jouer</button>
         <button class="btn">Recettes</button>
