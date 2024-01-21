06308f1e6074344cea06e510228a3368b71dbff2
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2024-01-21 18:58:02 +0100

    Refactor AtomCard and Game components

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -50,12 +50,13 @@ const atomsList = ref<AtomCardProps[]>([ {
 </script>
 
 <template>
-    <div class="min-h-[calc(100vh-4rem)] bg-red-500 flex overflow-hidden">
-        <div class="h-full w-2/3 bg-blue-500 border-r-2 border-black ">
-            a
+    <div class="min-h-[calc(100vh-4rem)] flex overflow-hidden">
+        <div class="min-h-full w-2/3 border-r-2 border-black ">
+            
         </div>
-        <div class="h-full w-1/3 flex flex-col gap-4  bg-blue-200 overflow-auto ">
+        <div class="min-h-full w-1/3 flex flex-row flex-wrap gap-6 p-6">
             <AtomCard v-for="atom in atomsList" :name="atom.name" :symbol="atom.symbol" :atomic-number="atom.atomicNumber"  />
         </div>
     </div>
+
 </template>
