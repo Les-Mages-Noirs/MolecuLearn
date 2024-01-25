4be01401f061e159b1d6c8c061c5229b5ed0b991
Ruben GAY <95344994+Ruben-GAY@users.noreply.github.com>
2024-01-25 19:18:29 +0100

    add User file with signUpForm

---
+++ src/api/user.ts
@@ -0,0 +1,26 @@
+import { API } from './api';
+import { User } from './types';
+
+interface GetUsersBody {
+    '@context': string;
+    '@id': string;
+    '@type': string;
+    'hydra:totalItems': number;
+    'hydra:member': User[];
+}
+type NewUser = {
+    email: string;
+    username: string;
+    password: string;
+};
+
+
+export const getUsers = async (): Promise<User[]> => {
+    const body = await API.get<GetUsersBody>('/users');
+    return body['hydra:member'];
+};
+
+export const addUser = async (user: NewUser): Promise<User> => {
+    const body = await API.post<User>('/users', user);
+    return body;
+}
