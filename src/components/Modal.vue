e24e97743e50214049cfbc1351a01690d9121743
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2024-01-28 16:24:28 +0100

    Refactor Modal component layout and styling

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -14,9 +14,13 @@ defineProps<{
 </script>
 <template>
 
-  <div class="p-6 absolute top-1/2 left-1/2 shadow-md rounded-md " >
-    <button @click="$emit('closeModal')"> X </button>
-    <div class="text-xl"> {{  molecule.name }} </div>
+  <div class="p-6 absolute top-1/3 left-1/3 border border-gray-200 rounded-md z-10 " >
+    <div class="p-2 flex items-center justify-between">
+        <div class="text-2xl font-bold"> {{  molecule.name }} </div>
+        <button class="btn" @click="$emit('closeModal')"> X </button>
+    </div>
+   
+    <div class="text-xl py-6"> qosidhfkqsldfmqlskdjhf lqsjdf mlkqsjdfmlkqjsdflkjqsdpf </div>
   </div>
 
 </template>
