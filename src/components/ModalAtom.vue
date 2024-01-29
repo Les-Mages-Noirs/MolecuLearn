188e4c733a945e3cf7499317d0ac7d5062a0f60e
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2024-01-29 02:13:16 +0100

    Refactor ModalAtom.vue component

--- src/components/ModalAtom.vue
+++ src/components/ModalAtom.vue
@@ -10,6 +10,7 @@ defineProps<{
   atom: Atom;
 }>();
 </script>
+
 <template>
   <div
     class="bg-white p-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-200 rounded-md z-10"
@@ -19,17 +20,44 @@ defineProps<{
       <button class="btn" @click="$emit('closeModal')">X</button>
     </div>
 
-    <div class="text-lg py-6">
-      <span class="text-gray-400"> Masse atomique :</span>
-      <span class="text-black"> {{ atom.atomicMass }}</span>
+    <div class="text-lg py-4">
+      <span class="text-gray-400">Numéro atomique : </span>
+      <span class="text-black font-semibold ml-2">{{ atom.atomicNumber }}</span>
     </div>
-    <div class="text-lg py-6">
-      <span class="text-gray-400"> Masse atomique :</span>
-      <span class="text-black"> {{ atom.atomicMass }}</span>
+
+    <div class="text-lg py-4">
+      <span class="text-gray-400">Symbole : </span>
+      <span class="text-black font-semibold ml-2">{{ atom.symbol }}</span>
     </div>
-    <div class="text-lg py-6">
-      <span class="text-gray-400"> Masse atomique :</span>
-      <span class="text-black"> {{ atom.atomicMass }}</span>
+
+    <div class="text-lg py-4">
+      <span class="text-gray-400">Point de fusion : </span>
+      <span class="text-black font-semibold ml-2">{{ atom.fusionPoint }}</span>
+    </div>
+
+    <div class="text-lg py-4">
+      <span class="text-gray-400">Point d'ébullition : </span>
+      <span class="text-black font-semibold ml-2">{{ atom.boilingPoint }}</span>
+    </div>
+
+    <div class="text-lg py-4">
+      <span class="text-gray-400">Masse atomique : </span>
+      <span class="text-black font-semibold ml-2">{{ atom.atomicMass }}</span>
+    </div>
+
+    <div class="text-lg py-4">
+      <span class="text-gray-400">Masse volumique : </span>
+      <span class="text-black font-semibold ml-2">{{ atom.volumicMass }}</span>
+    </div>
+
+    <div class="text-lg py-4">
+      <span class="text-gray-400">Découverte : </span>
+      <span class="text-black font-semibold ml-2">{{ atom.discovery }}</span>
+    </div>
+
+    <div class="text-lg py-4">
+      <span class="text-gray-400">Découvert par : </span>
+      <span class="text-black font-semibold ml-2">{{ atom.discoverer }}</span>
     </div>
   </div>
 </template>
